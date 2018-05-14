237d8ca5775d26d87b8e10e2dfa47f0728041989
YounHun Joung <yhyh0614@gmail.com>
2018-05-14 20:50:29 +0900

    edit vgg16 model

--- train.py
+++ train.py
@@ -6,6 +6,9 @@ from keras.models import Sequential
 from keras.layers import Dropout, Flatten, Dense, Activation
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras import callbacks
+from keras.optimizers import SGD
+from keras.applications.vgg16 import VGG16
+from keras import regularizers
 
 DEV = False
 argvs = sys.argv
@@ -15,84 +18,105 @@ if argc > 1 and (argvs[1] == '--development' or argvs[1] == '-d'):
 	DEV = True
 
 if DEV:
-	epochs = 2
-else:
 	epochs = 20
+else:
+	epochs = 100
 
 
 train_data_path = './data/train'
 validation_data_path = './data/validation'
 
-img_width, img_height = 224, 224
+img_height = 150 
+img_width = 150
+image_shape = (img_height, img_width, 3)
+weight_decay = 0.0005
+
 classes_num = 13
-lr = 0.0004
-
 batch_size = 32
-samples_per_epoch = 1000
-validation_steps = 300
-
-# model = Sequential()
-# model.add(Conv2D(32, (3, 3), padding ="same", input_shape=(img_width, img_height, 3)))
-# model.add(Activation("relu"))
-# model.add(MaxPooling2D(2, 2))
-
-# model.add(Conv2D(64, (2, 2), padding ="same"))
-# model.add(Activation("relu"))
-# model.add(MaxPooling2D(2, 2))
-
-# model.add(Flatten())
-# model.add(Dense(256))
-# model.add(Activation("relu"))
-# model.add(Dropout(0.5))
-# model.add(Dense(classes_num, activation='softmax'))
+samples_per_epoch = 300
+validation_steps = 90
 
 # VGG16
-model = Sequential()
+def build_model(image_shape, weight_decay, classes_num):
+    model = Sequential()
+    weight_decay = weight_decay
 
-# Block1
-model.add(Conv2D(64, (3, 3), padding='same', activation = 'relu', input_shape=(img_width, img_height, 3), name='block1_conv1'))
-model.add(Conv2D(64, (3, 3), padding='same', activation = 'relu', name='block1_conv2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
-# Block2
-model.add(Conv2D(128, (3, 3), padding='same', activation = 'relu', name='block2_conv1'))
-model.add(Conv2D(128, (3, 3), padding='same', activation = 'relu', name='block2_conv2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
-# Block3
-model.add(Conv2D(256, (3, 3), padding='same', activation = 'relu', name='block3_conv1'))
-model.add(Conv2D(256, (3, 3), padding='same', activation = 'relu', name='block3_conv2'))
-model.add(Conv2D(256, (3, 3), padding='same', activation = 'relu', name='block3_conv3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
-# Block4
-model.add(Conv2D(512, (3, 3), padding='same', activation = 'relu', name='block4_conv1'))
-model.add(Conv2D(512, (3, 3), padding='same', activation = 'relu', name='block4_conv2'))
-model.add(Conv2D(512, (3, 3), padding='same', activation = 'relu', name='block4_conv3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
-# Block5
-model.add(Conv2D(512, (3, 3), padding='same', activation = 'relu', name='block5_conv1'))
-model.add(Conv2D(512, (3, 3), padding='same', activation = 'relu', name='block5_conv2'))
-model.add(Conv2D(512, (3, 3), padding='same', activation = 'relu', name='block5_conv3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool'))
+    model.add(Conv2D(64, (3, 3), padding='same',
+                     input_shape=image_shape, kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.3))
 
-model.add(Flatten(name='flatten'))
-model.add(Dense(4096, activation='relu',name='fc1'))
-model.add(Dropout(0.5,name='dropout1'))
-model.add(Dense(4096, activation='relu',name='fc2'))
-model.add(Dropout(0.5,name='dropout2'))
-model.add(Dense(classes_num, activation='softmax', name='predictions'))
+    model.add(Conv2D(64, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
 
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# model training 과정
-model.compile(loss='categorical_crossentropy',
-              optimizer=optimizers.RMSprop(lr=lr),
-              metrics=['accuracy'])
+    model.add(Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(256, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(256, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(256, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+    model.add(Dense(512, kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('relu'))
+
+    model.add(Dropout(0.5))
+    model.add(Dense(classes_num))
+    model.add(Activation('softmax'))
+    return model
+
+# create model
+model = build_model(image_shape, weight_decay, classes_num)
+optimizer = SGD(lr=1e-3, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+model.summary()
+
 
 # Data pre_processing
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
-
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
     shear_range=0.2,
@@ -120,13 +144,17 @@ tb_cb = callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0)
 cbks = [tb_cb]
 
 # training model with generator
+# training
 model.fit_generator(
     train_generator,
     steps_per_epoch=samples_per_epoch,
     epochs=epochs,
-    validation_data=validation_generator,
+    verbose=1,
     callbacks=cbks,
-    validation_steps=validation_steps)
+    validation_data=validation_generator,
+    validation_steps=validation_steps,
+    max_queue_size=3
+)
 
 # Save model
 target_dir = './models/'
