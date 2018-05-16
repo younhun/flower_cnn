492e107665a8c7fb9c19e1e0a761698bb366dd76
YounHun Joung <yhyh0614@gmail.com>
2018-05-16 16:19:08 +0900

    add save weights

--- predict.py
+++ predict.py
@@ -5,9 +5,9 @@ from keras.models import Sequential, load_model
 
 img_width, img_height = 128, 128
 model_path = './models/model.h5'
-model_weights_path = './models/weights.h5'
+# model_weights_path = './models/weights.h5'
 model = load_model(model_path)
-model.load_weights(model_weights_path)
+# model.load_weights(model_weights_path)
 
 def predict(file):
   x = load_img(file, target_size=(img_width,img_height))
@@ -37,18 +37,19 @@ def predict(file):
   return answer
 
 
-0_t = 0
-0_f = 0
-1_t = 0
-1_f = 0
-2_t = 0
-2_f = 0
-3_t = 0
-3_f = 0
-4_t = 0
-4_f = 0
-5_t = 0
-5_f = 0
+n0_t = 0
+n0_f = 0
+n1_t = 0
+n1_f = 0
+n2_t = 0
+n2_f = 0
+n3_t = 0
+n3_f = 0
+n4_t = 0
+n4_f = 0
+n5_t = 0
+n5_f = 0
+
 
 for i, ret in enumerate(os.walk('./test-data/개나리')):
   for i, filename in enumerate(ret[2]):
@@ -65,9 +66,9 @@ for i, ret in enumerate(os.walk('./test-data/개나리')):
     
     # result에 따라 true, false 
     if result == 0:
-      0_t += 1
+      n0_t += 1
     else:
-      0_f += 1
+      n0_f += 1
 
 for i, ret in enumerate(os.walk('./test-data/동백')):
   for i, filename in enumerate(ret[2]):
@@ -75,9 +76,9 @@ for i, ret in enumerate(os.walk('./test-data/동백')):
       continue
     result = predict(ret[0] + '/' + filename)
     if result == 1:
-      1_t += 1
+      n1_t += 1
     else:
-      1_f += 1
+      n1_f += 1
 
 for i, ret in enumerate(os.walk('./test-data/목화')):
   for i, filename in enumerate(ret[2]):
@@ -85,9 +86,9 @@ for i, ret in enumerate(os.walk('./test-data/목화')):
       continue
     result = predict(ret[0] + '/' + filename)
     if result == 1:
-      2_t += 1
+      n2_t += 1
     else:
-      2_f += 1
+      n2_f += 1
 
 for i, ret in enumerate(os.walk('./test-data/백합')):
   for i, filename in enumerate(ret[2]):
@@ -95,9 +96,9 @@ for i, ret in enumerate(os.walk('./test-data/백합')):
       continue
     result = predict(ret[0] + '/' + filename)
     if result == 1:
-      3_t += 1
+      n3_t += 1
     else:
-      3_f += 1
+      n3_f += 1
 
 for i, ret in enumerate(os.walk('./test-data/안투리움')):
   for i, filename in enumerate(ret[2]):
@@ -105,9 +106,9 @@ for i, ret in enumerate(os.walk('./test-data/안투리움')):
       continue
     result = predict(ret[0] + '/' + filename)
     if result == 2:
-      4_t += 1
+      n4_t += 1
     else:
-      4_f += 1
+      n4_f += 1
 
 for i, ret in enumerate(os.walk('./test-data/장미')):
   for i, filename in enumerate(ret[2]):
@@ -115,22 +116,22 @@ for i, ret in enumerate(os.walk('./test-data/장미')):
       continue
     result = predict(ret[0] + '/' + filename)
     if result == 2:
-      5_t += 1
+      n5_t += 1
     else:
-      5_f += 1
+      n5_f += 1
 
 # """
 # Check metrics
 # """
-print("True 개나리: ", 0_t)
-print("False 개나리: ", 0_f)
-print("True 동백: ", 1_t)
-print("False 동백: ", 1_f)
-print("True 목화: ", 2_t)
-print("False 목화: ", 2_f)
-print("True 백합: ", 3_t)
-print("False 백합: ", 3_f)
-print("True 안투리움: ", 4_t)
-print("False 안투리움: ", 4_f)
-print("True 장미: ", 5_t)
-print("False 장미: ", 5_f)
+print("True 개나리: ", n0_t)
+print("False 개나리: ", n0_f)
+print("True 동백: ", n1_t)
+print("False 동백: ", n1_f)
+print("True 목화: ", n2_t)
+print("False 목화: ", n2_f)
+print("True 백합: ", n3_t)
+print("False 백합: ", n3_f)
+print("True 안투리움: ", n4_t)
+print("False 안투리움: ", n4_f)
+print("True 장미: ", n5_t)
+print("False 장미: ", n5_f)
--- train.py
+++ train.py
@@ -142,6 +142,6 @@ target_dir = './models/'
 if not os.path.exists(target_dir):
   os.mkdir(target_dir)
 model.save('./models/model.h5')
-
+model.save_weights('./models/weights.h5')
 
 
