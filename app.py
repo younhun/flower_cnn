01d4c35b22599ee6554be0baa334ce8703bb1246
YounHun Joung <yhyh0614@gmail.com>
2018-05-20 02:57:39 +0900

    add app.py

--- app.py
+++ app.py
@@ -1,61 +1,20 @@
-import os
- 
-from flask import Flask, render_template, request, redirect, url_for
+# -- coding: utf-8 --
+
+from firebase import firebase
+from flask import Flask, jsonify, request, render_template, Response, make_response, send_from_directory
+from flask_restful import Resource, Api
 from werkzeug import secure_filename
-from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
-from keras.models import Sequential, load_model
-import numpy as np
-import argparse
-import time
+import os
+import json
 import uuid
-import base64
-import tensorflow as tf
+from werkzeug import SharedDataMiddleware
 
-# keras 와 tensorflow graph충돌
-#############################
-graph = tf.get_default_graph()
-#############################
+import predict
 
-img_width, img_height = 200, 180
-model_path = './models/model.h5'
-model_weights_path = './models/weights.h5'
-model = load_model(model_path)
-model.load_weights(model_weights_path)
 
-UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+UPLOAD_FOLDER = 'uploads'
 
-
-def get_as_base64(url):
-    return base64.b64encode(requests.get(url).content)
-
-def predict(file):
-    global graph
-    
-    x = load_img(file, target_size=(img_width,img_height))
-    x = img_to_array(x)
-    x = np.expand_dims(x, axis=0)
-    
-    ############################
-    with graph.as_default(): 
-        array = model.predict(x)
-    ############################
-    
-    result = array[0]
-    answer = np.argmax(result)
-    if answer == 0:
-        print("Label: 개나리")
-    elif answer == 1:
-        print("Label: 동백")
-    elif answer == 2:
-        print("Label: 목화")
-    elif answer == 3:
-        print("Label: 백합")
-    elif answer == 4:
-        print("Label: 안투리움")
-    elif answer == 5:
-        print("Label: 장미")
-    return answer
 
 def my_random_string(string_length=10):
     """Returns a random string of length string_length."""
@@ -69,67 +28,83 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-#########################################################################
+
+firebase = firebase.FirebaseApplication("https://flower-87ee2.firebaseio.com", None)
 
 app = Flask(__name__)
+api = Api(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-# @app.route('/user/<username>')
-# def show_user_profile(username):
-#     return 'user %s' % username
 
 @app.route("/")
 def template_test():
-    return render_template('template.html', label='', imagesource='../uploads/template.jpg')
+    return render_template('template.html', label='', imagesource='../uploads/main.jpg')
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        import time
-        start_time = time.time()
-        file = request.files['file']
+class Upload(Resource):	
+	def post(self):
+		file = request.files['file']
+		if file and allowed_file(file.filename):
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+			# file을 전송된 폼 데이터 위조를 방지, 파일명 보호
+			filename = secure_filename(file.filename)
+			file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+			file.save(file_path)
 
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            result = predict(file_path)
-            if result == 0:
-                label = '개나리'
-            elif result == 1:
-                label = '동백'			
-            elif result == 2:
-                label = '목화'
-            elif result == 3:
-                label = '백합'
-            elif result == 4:
-                label = '안투리움'
-            elif result == 5:
-                label = '장미'
-            
-            print(result)
-            print(file_path)
-            filename = my_random_string(6) + filename
+			filename = my_random_string(6) + filename
+			os.rename(file_path, os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            os.rename(file_path, os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print("--- %s seconds ---" % str (time.time() - start_time))
-            return render_template('template.html', label=label, imagesource='../uploads/' + filename)
+			result = predict.predict(file)
 
-from flask import send_from_directory
+			if result == 0:
+			    label = "안투리움"
+			elif result == 1:
+			    label =  "Ball Moss"
+			elif result == 2:
+			    label = '참매발톱'
+			elif result == 3:
+			    label = '가자니아'
+			elif result == 4:
+			    label = "장미"
+			elif result == 5:
+			    label = "해바라기"
+			elif result == 6:
+			    label = "wall flower"
+			elif result == 7:
+			    label = "yellow iris"
+
+			print(label)
+
+
+		upload = firebase.put('/uploads','/flower', {'image':filename, 'label' : label})
+
+		return jsonify({'message' : 'image upload success!'})
+
+class Load(Resource):
+	def get(self):
+		getImage = firebase.get('/uploads' + '/flower', 'image')
+		getLabel = firebase.get('/uploads' + '/flower', 'label')
+
+		# Flask 한글 json 유니코드 에러 해결
+		# getLabel = json.dumps(getLabel, ensure_ascii=False).encode('utf8')
+		# getLabel = jsonify(getLabel, content_type='application/json; charset=utf-8')
+
+		return jsonify('flower',{'image': getImage, 'label': getLabel})
+
+
+api.add_resource(Upload,'/api/upload')
+api.add_resource(Load,'/api/load')
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+	filename = firebase.get('/uploads' + '/flower', 'image')
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-from werkzeug import SharedDataMiddleware
 app.add_url_rule('/uploads/<filename>', 'uploaded_file',
                  build_only=True)
+
 app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
     '/uploads':  app.config['UPLOAD_FOLDER']
 })
+
 
 if __name__ == "__main__":
     app.debug=True
---
+++ test.py
@@ -0,0 +1,170 @@
+import os
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+
+img_width = 150
+img_height = 150
+
+model_path = './models/model.h5'
+model_weights_path = './models/weights.h5'
+model = load_model(model_path)
+model.load_weights(model_weights_path)
+
+def predict(file):
+  x = load_img(file, target_size=(img_width,img_height))
+  x = img_to_array(x)
+  x = np.expand_dims(x, axis=0)
+  
+  # one-hot encoding
+  array = model.predict(x)
+  result = array[0]
+  
+  # one-hot encoding 결과 값 추출
+  # 각 결과 값 label에 matching
+  answer = np.argmax(result)
+  if answer == 0:
+    print("Label: 안투리움")
+  elif answer == 1:
+    print("Label: Ball Moss")
+  elif answer == 2:
+    print("Label: 참매발톱")
+  elif answer == 3:
+    print("Label: 가자니아")
+  elif answer == 4:
+    print("Label: 장미")
+  elif answer == 5:
+    print("Label: 해바라기")
+  elif answer == 6:
+    print("Label: wall flower")
+  elif answer == 7:
+    print("Label : yellow iris")
+  return answer
+
+
+n0_t = 0
+n0_f = 0
+n1_t = 0
+n1_f = 0
+n2_t = 0
+n2_f = 0
+n3_t = 0
+n3_f = 0
+n4_t = 0
+n4_f = 0
+n5_t = 0
+n5_f = 0
+n6_t = 0
+n6_f = 0
+n7_t = 0
+n7_f = 0
+
+
+for i, ret in enumerate(os.walk('./test-data/anthurium')):
+  for i, filename in enumerate(ret[2]):
+    # ret[2] = folder에 들어있는 images들을 가리킨다.
+    
+    # DS_Store 처리
+    if filename.startswith("."):
+      continue
+    
+    # ret[0] = image 경로
+    # ret[0] + / + filename = 경로에 들어있는 image
+    # 경로의 image를 predict 함수에 넣는다.
+    result = predict(ret[0] + '/' + filename)
+    
+    # result에 따라 true, false 
+    if result == 0:
+      n0_t = n0_t + 1
+    else:
+      n0_f = n0_t + 1
+
+for i, ret in enumerate(os.walk('./test-data/ball moss')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+    result = predict(ret[0] + '/' + filename)
+    if result == 1:
+      n1_t = n1_t + 1
+    else:
+      n1_f = n1_f + 1
+
+for i, ret in enumerate(os.walk('./test-data/columbine')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+    result = predict(ret[0] + '/' + filename)
+    if result == 2:
+      n2_t = n2_t + 1
+    else:
+      n2_f = n2_f + 1
+
+for i, ret in enumerate(os.walk('./test-data/gazania')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+    result = predict(ret[0] + '/' + filename)
+    if result == 3:
+      n3_t = n3_t + 1
+    else:
+      n3_f = n3_f + 1
+
+for i, ret in enumerate(os.walk('./test-data/rose')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+    result = predict(ret[0] + '/' + filename)
+    if result == 4:
+      n4_t = n4_t + 1
+    else:
+      n4_f = n4_f + 1
+
+for i, ret in enumerate(os.walk('./test-data/sunflower')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+    result = predict(ret[0] + '/' + filename)
+    if result == 5:
+      n5_t = n5_t +  1
+    else:
+      n5_f = n5_f + 1
+
+for i, ret in enumerate(os.walk('./test-data/wallflower')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+    result = predict(ret[0] + '/' + filename)
+    if result == 6:
+      n6_t = n6_t +  1
+    else:
+      n6_f = n6_f + 1
+
+for i, ret in enumerate(os.walk('./test-data/yellow iris')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+    result = predict(ret[0] + '/' + filename)
+    if result == 7:
+      n7_t = n7_t +  1
+    else:
+      n7_f = n7_f + 1
+
+# """
+# Check metrics
+# """
+print("True anthurium: ", n0_t)
+print("False anthurium: ", n0_f)
+print("True ball moss: ", n1_t)
+print("False ball moss: ", n1_f)
+print("True columbine: ", n2_t)
+print("False columbine: ", n2_f)
+print("True gazania: ", n3_t)
+print("False gazania: ", n3_f)
+print("True rose: ", n4_t)
+print("False rose: ", n4_f)
+print("True sunflower: ", n5_t)
+print("False sunflower: ", n5_f)
+print("True wallflower: ", n6_t)
+print("False wallflower: ", n6_f)
+print("True yellow iris: ", n7_t)
+print("False yellow iris: ", n7_f)
